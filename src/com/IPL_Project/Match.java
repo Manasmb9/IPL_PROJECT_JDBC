2272a19ed88442d7c4a98e1b908e45aefc1e5aae
Manasmb9 <danda.manas.22@mountblue.tech>
2022-11-21 21:40:37 +0530

    added getter and setter method in match class

--- src/com/IPL_Project/Match.java
+++ src/com/IPL_Project/Match.java
@@ -1,5 +1,106 @@
 package com.IPL_Project;
 
 public class Match {
+    private int id;
+    private int season;
+    private String city;
+    private String date;
+    private String team1;
+    private String team2;
+    private String tossWinner;
+    private String tossDecision;
+    private String result;
+    private int dl_applied;
+    private String winner;
+    private int win_by_runs;
+    private int win_by_wickets;
+    private String player_of_match;
+
+    private String venue;
+
+    private String umpire1;
+    private String umpire2;
+
+
+    public int getId(){return id;}
+    public int getSeason() {
+        return season;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getDate() {
+        return date;
+    }
+    public String getTeam1() {
+        return team1;
+    }
+    public String getTeam2() {
+        return team2;
+    }
+    public String getTossWinner() {
+        return tossWinner;
+    }
+    public String getTossDecision() {
+        return tossDecision;
+    }
+
+    public String getResult() {
+        return result;
+    }
+    public int getDl_applied(){ return dl_applied;}
+    public String getWinner() {
+        return winner;
+    }
+    public int getWin_by_runs(){return win_by_runs;}
+    public int getWin_by_wickets(){return win_by_wickets;}
+    public String getPlayer_of_match(){return player_of_match;}
+    public String getVenue(){return venue;}
+    public String getUmpire1(){return umpire1;}
+    public String getUmpire2(){return umpire2;}
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setSeason(int season) {
+        this.season = season;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+    public void setTossWinner(String tossWinner) {
+        this.tossWinner = tossWinner;
+    }
+    public void setTossDecision(String tossDecision) {
+        this.tossDecision = tossDecision;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+    public void setDl_applied(int dl_applied){this.dl_applied = dl_applied;}
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public void setWin_by_runs(int win_by_runs){this.win_by_runs = win_by_runs;}
+
+    public void setWin_by_wickets(int win_by_wickets){this.win_by_wickets = win_by_wickets;}
+
+    public void setPlayer_of_match(String player_of_match){this.player_of_match = player_of_match;}
+
+    public void setVenue(String venue){this.venue = venue;}
+
+    public void setUmpire1(String umpire1){this.umpire1 = umpire1;}
+
+    public void setUmpire2(String umpire2){this.umpire2 = umpire2;}
 
 }
